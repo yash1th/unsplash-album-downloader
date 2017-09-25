7520509a89dd46dd54219100a7017d6854bda441
yamara <yklsga@gmail.com>
2017-09-25 11:53:59 -0400

    removed scratch work files

--- sample/sample_3.py
+++
@@ -1,4 +0,0 @@
-import requests
-base_url = 'https://api.unsplash.com/photos/users/jilebi/client_id=b2b72af949a8f7f5cddfcadcebcbbbd8981be1d99d30261ea5deebf05c7fb54a'
-r = requests.get(base_url, headers = {'Accept-Version':'v1'})
-print(r.status_code)
--- sample/test.py
+++
@@ -1,5 +0,0 @@
-import os
-print(os.getcwd())
-#path = r'/yaswanth amara'
-#if not os.path.exists(path):
-#    os.makedirs(path)
