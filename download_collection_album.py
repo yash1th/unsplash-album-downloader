49750a4dffde01bf01cd6af463976aaa11251aab
yash <yash@yash-mbp.local>
2020-01-29 09:11:50 -0800

    reformatted code

--- download_collection_album.py
+++ download_collection_album.py
@@ -3,29 +3,33 @@ from download_user_album import *
 COLLECTION_URL = 'https://api.unsplash.com/collections/'
 
 
-def get_collections(cid, mode, curated_flag = False):
+def get_collections(cid, mode, curated_flag=False):
     title = '-unsplash-'
     if curated_flag:
-        collection, s = get_response(COLLECTION_URL+'curated/'+cid,{'client_id':APP_ID})
-        title = '-curated'+title
+        collection, s = get_response(COLLECTION_URL + 'curated/' + cid, {'client_id': APP_ID})
+        title = '-curated' + title
     else:
-        collection, s = get_response(COLLECTION_URL+cid,{'client_id':APP_ID})
-        title = title+'collection-'
-    photo_ids = get_photo_ids(collection['links']['photos'],collection['total_photos'], mode)
-    user_directory = os.getcwd()+r'/'+collection['user']['name']+title+collection['title'].replace('/', '_')+'-'+mode #replaces / with underscore
+        collection, s = get_response(COLLECTION_URL + cid, {'client_id': APP_ID})
+        title = title + 'collection-'
+    photo_ids = get_photo_ids(collection['links']['photos'], collection['total_photos'], mode)
+    user_directory = os.getcwd() + r'/' + collection['user']['name'] + title + collection['title'].replace('/',
+                                                                                                           '_') + '-' + mode  # replaces / with underscore
     save_photos(user_directory, photo_ids)
+
 
 def collection_main():
     args = collection_parse_args()
     get_collections(args.collection_id, args.size, args.curated)
 
+
 def collection_parse_args():
-    parser = argparse.ArgumentParser(description = 'arguments of how to download collections')
-    parser.add_argument('collection_id', type = str.lower, help = 'id of the collection that needs to be downloaded')
-    parser.add_argument('-c','--curated', action = 'store_true', help = 'provide if its a curated collection')
-    parser.add_argument('size',type=str.lower, nargs = '?',  default = 'regular',
-                        choices = ['raw','full','regular','small','thumb'])
+    parser = argparse.ArgumentParser(description='arguments of how to download collections')
+    parser.add_argument('collection_id', type=str.lower, help='id of the collection that needs to be downloaded')
+    parser.add_argument('-c', '--curated', action='store_true', help='provide if its a curated collection')
+    parser.add_argument('size', type=str.lower, nargs='?', default='regular',
+                        choices=['raw', 'full', 'regular', 'small', 'thumb'])
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     collection_main()
